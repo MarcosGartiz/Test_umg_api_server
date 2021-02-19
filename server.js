e7aa0a6c3159fd6a96d053525a86c23da773aae4
Marcos-umg <marcos.garciatiznado@umg.edu.mx>
2021-02-19 11:44:05 -0600

    Update

--- server.js
+++ server.js
@@ -95,11 +95,11 @@ app.post('/solve-sum',function(req, res) {
 });
 
 app.post('/dial/code',function(req, res) {
-	var aux = req.body.want;
+	var aux = req.body.dialcode;
 	var URL = 'https://api.toys/api/dialing_codes/'+aux;
 	
 	axios.post(URL,	{
-		want: aux,
+		dialcode: aux,
 	}).then((response)=>{
 		res.send(response.data)
 	}).catch((error) =>{
